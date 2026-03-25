a7041929e2b85f911ada45aff318a1a7dba247c9
Alonso Luque <jorge.luquem@unmsm.edu.pe>
2026-03-25 17:32:32 -0500

    fix(checkpointer): corregir lógica de guardado

--- src/contractai_backend/modules/chatbot/infrastructure/agent/checkpointer.py
+++ src/contractai_backend/modules/chatbot/infrastructure/agent/checkpointer.py
@@ -1,18 +1,22 @@
 from psycopg_pool import AsyncConnectionPool
+from psycopg import AsyncConnection
 from psycopg.rows import dict_row
 from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
 
 from contractai_backend.shared.config import settings
 
+async def setup_connection(conn: AsyncConnection) -> None:
+    await conn.execute("SET search_path TO checkpoint, public;")
+
 async def init_checkpointer() -> AsyncConnectionPool:
     pool = AsyncConnectionPool(
         conninfo=settings.CONN_STRING,
         open=False,
+        configure=setup_connection,
         kwargs={
             "prepare_threshold": 0,
             "row_factory": dict_row,
-            "autocommit": True,
-            "options": "-c search_path=checkpoint,public"
+            "autocommit": True
         }
     )
 
